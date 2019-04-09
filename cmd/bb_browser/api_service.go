c82df683f0336d9d551899df5ca6fd8f9d2a697e
Ed Schouten <ed.schouten@prodrive-technologies.com>
2019-04-09 13:34:43 +0200

    Expose command messages.

--- cmd/bb_browser/api_service.go
+++ cmd/bb_browser/api_service.go
@@ -21,6 +21,7 @@ func NewAPIService(contentAddressableStorage cas.ContentAddressableStorage, rout
 	s := &APIService{
 		contentAddressableStorage: contentAddressableStorage,
 	}
+	router.HandleFunc("/api/get_command", s.handleGetCommand)
 	router.HandleFunc("/api/get_directory", s.handleGetDirectory)
 	return s
 }
@@ -37,6 +38,22 @@ func getDigestFromQueryParameters(req *http.Request) (*util.Digest, error) {
 			Hash:      vars.Get("hash"),
 			SizeBytes: sizeBytes,
 		})
+}
+
+func (s *APIService) handleGetCommand(w http.ResponseWriter, req *http.Request) {
+	digest, err := getDigestFromQueryParameters(req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	directory, err := s.contentAddressableStorage.GetCommand(req.Context(), digest)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if err := s.marshaler.Marshal(w, directory); err != nil {
+		log.Print(err)
+	}
 }
 
 func (s *APIService) handleGetDirectory(w http.ResponseWriter, req *http.Request) {
