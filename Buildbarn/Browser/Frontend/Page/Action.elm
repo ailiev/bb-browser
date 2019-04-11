89ef1b5c1c949e35942fc3c455d3d247fd2f1d6a
Ed Schouten <ed.schouten@prodrive-technologies.com>
2019-04-11 23:16:54 +0200

    Work towards implementing the action page.

--- Buildbarn/Browser/Frontend/Page/Action.elm
+++ Buildbarn/Browser/Frontend/Page/Action.elm
@@ -4,7 +4,8 @@ import Build.Bazel.Remote.Execution.V2.Remote_execution as REv2
 import Buildbarn.Browser.Frontend.Api as Api
 import Buildbarn.Browser.Frontend.Page as Page
 import Buildbarn.Browser.Frontend.Route as Route
-import Html exposing (p, text)
+import Html exposing (h2, p, table, td, text, th, tr)
+import Html.Attributes exposing (class, style)
 import Http
 
 
@@ -12,13 +13,15 @@ import Http
 -- MODEL
 
 
-type Model
-    = Loading
+type alias Model =
+    { action : Maybe (Api.CallResult REv2.Action)
+    , actionResult : Maybe (Api.CallResult REv2.ActionResult)
+    }
 
 
 init : Route.Digest -> ( Model, Cmd Msg )
 init digest =
-    ( Loading
+    ( { action = Nothing, actionResult = Nothing }
     , Cmd.batch
         [ Api.getMessage "action" GotAction REv2.actionDecoder digest
         , Api.getMessage "action_result" GotActionResult REv2.actionResultDecoder digest
@@ -37,7 +40,12 @@ type Msg
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        GotAction action ->
+            ( { model | action = Just action }, Cmd.none )
+
+        GotActionResult actionResult ->
+            ( { model | actionResult = Just actionResult }, Cmd.none )
 
 
 
@@ -47,6 +55,28 @@ update msg model =
 view : Model -> Page.Page msg
 view model =
     { title = "Action"
-    , bannerColor = "secondary"
-    , body = []
+    , bannerColor =
+        case model.actionResult of
+            Just (Ok actionResult) ->
+                if actionResult.exitCode == 0 then
+                    "success"
+
+                else
+                    "danger"
+
+            _ ->
+                "secondary"
+    , body =
+        (Page.viewApiCallResult model.action <| \_ -> [])
+            ++ [ h2 [] [ text "Result " ] ]
+            ++ (Page.viewApiCallResult model.actionResult <|
+                    \actionResult ->
+                        [ table [ class "table", style "table-layout" "fixed" ] <|
+                            [ tr []
+                                [ th [ style "width" "25%" ] [ text "Exit code:" ]
+                                , td [ style "width" "75%" ] [ text <| String.fromInt actionResult.exitCode ]
+                                ]
+                            ]
+                        ]
+               )
     }
