8828678077a9feaf4cc13b09043e67e41632c297
Ed Schouten <ed.schouten@prodrive-technologies.com>
2019-04-18 17:37:46 +0200

    Maybe fetch output files?

--- Buildbarn/Browser/Frontend/Page/Action.elm
+++ Buildbarn/Browser/Frontend/Page/Action.elm
@@ -13,12 +13,14 @@ import Build.Bazel.Remote.Execution.V2.Remote_execution as REv2
 import Buildbarn.Browser.Frontend.Api as Api
 import Buildbarn.Browser.Frontend.Digest exposing (Digest)
 import Buildbarn.Browser.Frontend.Page as Page
+import Bytes exposing (Bytes)
 import Google.Protobuf.Duration as Duration
-import Html exposing (a, h2, p, sup, table, td, text, th, tr)
+import Html exposing (Html, a, h2, p, sup, table, td, text, th, tr)
 import Html.Attributes exposing (class, href, style)
 import Http
 import Json.Decode as JD
 import Pkg.Proto.Cas.Cas as Cas
+import Url.Builder
 
 
 
@@ -27,7 +29,7 @@ import Pkg.Proto.Cas.Cas as Cas
 
 type alias Model =
     { action : Maybe (Api.CallResult ActionModel)
-    , actionResult : Maybe (Api.CallResult REv2.ActionResult)
+    , actionResult : Maybe (Api.CallResult ActionResultModel)
     }
 
 
@@ -35,6 +37,13 @@ type alias ActionModel =
     { data : REv2.Action
     , command : Maybe (Api.CallResult REv2.Command)
     , inputRoot : Maybe (Api.CallResult REv2.Directory)
+    }
+
+
+type alias ActionResultModel =
+    { data : REv2.ActionResult
+    , stderr : Maybe (Result Http.Error String)
+    , stdout : Maybe (Result Http.Error String)
     }
 
 
@@ -64,7 +73,43 @@ type Msg
     | GotActionResult Digest (Api.CallResult REv2.ActionResult)
     | GotCommand Digest (Api.CallResult REv2.Command)
     | GotInputRoot Digest (Api.CallResult REv2.Directory)
+    | GotStderr (Result Http.Error String)
+    | GotStdout (Result Http.Error String)
     | GotUncachedActionResult Digest (Api.CallResult Cas.UncachedActionResult)
+
+
+getCmdForStream : (Result Http.Error String -> msg) -> Bytes -> Maybe REv2.DigestMessage -> Cmd msg
+getCmdForStream toMsg raw maybeDigest =
+    if Bytes.width raw > 0 then
+        Cmd.none
+
+    else
+        case maybeDigest of
+            Just (REv2.DigestMessage digest) ->
+                -- TODO: Add size limit.
+                Http.get
+                    { url =
+                        Url.Builder.relative
+                            [ "file "
+                            , "TODO"
+                            , digest.hash
+                            , String.fromInt digest.sizeBytes
+                            , "log.txt"
+                            ]
+                            []
+                    , expect = Http.expectString toMsg
+                    }
+
+            _ ->
+                Cmd.none
+
+
+getCmdsForActionResult : REv2.ActionResult -> Cmd Msg
+getCmdsForActionResult actionResult =
+    Cmd.batch
+        [ getCmdForStream GotStderr actionResult.stderrRaw actionResult.stderrDigest
+        , getCmdForStream GotStdout actionResult.stdoutRaw actionResult.stdoutDigest
+        ]
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -106,9 +151,23 @@ update msg model =
         GotActionResult _ actionResult ->
             ( model
                 |> (mapFieldActionResult <|
-                        \_ -> Just actionResult
+                        \_ ->
+                            Just <|
+                                Result.map
+                                    (\actionResultMessage ->
+                                        { data = actionResultMessage
+                                        , stdout = Nothing
+                                        , stderr = Nothing
+                                        }
+                                    )
+                                    actionResult
                    )
-            , Cmd.none
+            , case actionResult of
+                Ok actionResultMessage ->
+                    getCmdsForActionResult actionResultMessage
+
+                _ ->
+                    Cmd.none
             )
 
         GotCommand _ command ->
@@ -133,6 +192,28 @@ update msg model =
             , Cmd.none
             )
 
+        GotStderr body ->
+            ( model
+                |> (mapFieldActionResult <|
+                        Maybe.map <|
+                            Result.map <|
+                                mapFieldStderr <|
+                                    \a -> Just body
+                   )
+            , Cmd.none
+            )
+
+        GotStdout body ->
+            ( model
+                |> (mapFieldActionResult <|
+                        Maybe.map <|
+                            Result.map <|
+                                mapFieldStdout <|
+                                    \a -> Just body
+                   )
+            , Cmd.none
+            )
+
         GotUncachedActionResult uncachedActionResultDigest uncachedActionResult ->
             ( case uncachedActionResult of
                 Err e ->
@@ -144,7 +225,16 @@ update msg model =
                             model
 
                         Just (REv2.ActionResultMessage actionResult) ->
-                            { model | actionResult = Just (Ok actionResult) }
+                            { model
+                                | actionResult =
+                                    Just
+                                        (Ok
+                                            { data = actionResult
+                                            , stderr = Nothing
+                                            , stdout = Nothing
+                                            }
+                                        )
+                            }
             , Api.getChildMessage
                 "action"
                 GotAction
@@ -175,6 +265,16 @@ mapFieldInputRoot updater record =
     { record | inputRoot = updater record.inputRoot }
 
 
+mapFieldStderr : (a -> a) -> { b | stderr : a } -> { b | stderr : a }
+mapFieldStderr updater record =
+    { record | stderr = updater record.stderr }
+
+
+mapFieldStdout : (a -> a) -> { b | stdout : a } -> { b | stdout : a }
+mapFieldStdout updater record =
+    { record | stdout = updater record.stdout }
+
+
 
 -- VIEW
 
@@ -185,7 +285,7 @@ view model =
     , bannerColor =
         case model.actionResult of
             Just (Ok actionResult) ->
-                if actionResult.exitCode == 0 then
+                if actionResult.data.exitCode == 0 then
                     "success"
 
                 else
@@ -237,9 +337,9 @@ view model =
                             [ tr []
                                 [ th [ style "width" "25%" ] [ text "Exit code:" ]
                                 , td [ style "width" "75%" ]
-                                    [ text <| String.fromInt actionResult.exitCode
+                                    [ text <| String.fromInt actionResult.data.exitCode
                                     , text " "
-                                    , if actionResult.exitCode == 0 then
+                                    , if actionResult.data.exitCode == 0 then
                                         Badge.badgeSuccess [] [ text "Success " ]
 
                                       else
@@ -284,7 +384,7 @@ view model =
                                                 entry.treeDigest
                                                 [ text entry.path ]
                                         )
-                                        actionResult.outputDirectories
+                                        actionResult.data.outputDirectories
                                         ++ List.map
                                             (\(REv2.OutputSymlinkMessage entry) ->
                                                 Page.viewDirectoryListingEntry
@@ -292,7 +392,7 @@ view model =
                                                     Nothing
                                                     [ text entry.path ]
                                             )
-                                            actionResult.outputDirectorySymlinks
+                                            actionResult.data.outputDirectorySymlinks
                                         ++ List.map
                                             (\(REv2.OutputFileMessage entry) ->
                                                 Page.viewDirectoryListingEntry
@@ -305,7 +405,7 @@ view model =
                                                     entry.digest
                                                     [ text entry.path ]
                                             )
-                                            actionResult.outputFiles
+                                            actionResult.data.outputFiles
                                         ++ List.map
                                             (\(REv2.OutputSymlinkMessage entry) ->
                                                 Page.viewDirectoryListingEntry
@@ -313,7 +413,7 @@ view model =
                                                     Nothing
                                                     [ text entry.path ]
                                             )
-                                            actionResult.outputFileSymlinks
+                                            actionResult.data.outputFileSymlinks
 
                                 Nothing ->
                                     []
